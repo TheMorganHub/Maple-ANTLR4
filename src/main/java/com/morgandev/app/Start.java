59b4fe7d936addbca17a1780862e341f46d01ecf
Morgan <snow.eisen@gmail.com>
2019-05-01 22:22:18 -0300

    improvements to cli execution

--- src/main/java/com/morgandev/app/Start.java
+++ src/main/java/com/morgandev/app/Start.java
@@ -7,26 +7,46 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.util.Scanner;
+
 public class Start {
 
     public static void main(String[] args) {
         try {
-            String mapleStatement = "personas(nombre, apellido) <<- 'David' ? id < 5;";
             if (args.length > 0) {
-                mapleStatement = args[0];
+                startFromCmd();
+            } else {
+                String mapleStatement = "personas(nombre) <<- 'David' ? id < 5;";
+                start(mapleStatement);
             }
-            CharStream cs = CharStreams.fromString(mapleStatement);
-            MapleLexer lexer = new MapleLexer(cs);
-            CommonTokenStream token = new CommonTokenStream(lexer);
-            MapleParser parser = new MapleParser(token);
-            ParseTree tree = parser.parse();
-
-            MapleCustomVisitor visitor = new MapleCustomVisitor();
-            String val = visitor.visit(tree);
-            System.out.println(val);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void start(String mapleStatement) {
+        CharStream cs = CharStreams.fromString(mapleStatement);
+        MapleLexer lexer = new MapleLexer(cs);
+        CommonTokenStream token = new CommonTokenStream(lexer);
+        MapleParser parser = new MapleParser(token);
+        ParseTree tree = parser.parse();
+
+        MapleCustomVisitor visitor = new MapleCustomVisitor();
+        String val = visitor.visit(tree);
+        System.out.println(val);
+    }
+
+    public static void startFromCmd() {
+        Scanner scanner = new Scanner(System.in);
+        String mapleStatement;
+        do {
+            System.out.println("Enter a Maple statement:\n");
+            mapleStatement = scanner.nextLine();
+            if (!mapleStatement.equals("exit")) {
+                start(mapleStatement);
+            } else {
+                System.out.println("Bye!");
+            }
+        } while (!mapleStatement.equals("exit"));
     }
 }
